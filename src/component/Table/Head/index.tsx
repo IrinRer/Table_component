ee574291959e7e3351c9c3efe24857ae5837111b
Irin <iraantonova1998@list.ru>
2022-09-24 11:55:03 +0300

    thead table

--- src/component/Table/Head/index.tsx
+++ src/component/Table/Head/index.tsx
@@ -3,15 +3,17 @@ import styles from './index.module.scss';
 
 const Head = () => {
   return (
-    <tr>
-      <th>Project</th>
-      <th>Token Type</th>
-      <th>Conditions</th>
-      <th>Volume</th>
-      <th>ROI</th>
-      <th>Free float</th>
-      <th>Insuranse hedge</th>
-    </tr>
+    <thead>
+      <tr>
+        <th>Project</th>
+        <th>Token Type</th>
+        <th>Conditions</th>
+        <th>Volume</th>
+        <th>ROI</th>
+        <th>Free float</th>
+        <th>Insuranse hedge</th>
+      </tr>
+    </thead>
   );
 };
 
