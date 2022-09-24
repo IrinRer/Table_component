0b157b96a5926b4c4a7f0a05045c3404ba4d6480
Irin <iraantonova1998@list.ru>
2022-09-24 11:40:22 +0300

    thunk for data

---
+++ src/store/data/thunk.ts
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { AxiosResponse } from 'axios';
+import { api } from 'network';
+import { DATA_SLICE_ALIAS } from './types';
+
+export const fechDataAction = createAsyncThunk(
+  `${DATA_SLICE_ALIAS}/fetch`,
+  async (_, { rejectWithValue }) => {
+    try {
+      const response: AxiosResponse = await api().get('/data');
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
