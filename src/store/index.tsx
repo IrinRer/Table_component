8952f7f15cfde9e45befc9a9fbbf5af4caa1ebaf
Irin <iraantonova1998@list.ru>
2022-09-24 11:42:20 +0300

    change store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import DataReducer from './data/slice';
+
+import { IDataSlice } from './data/types';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { data: DataReducer },
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-export type RootState = {};
+export type RootState = {
+  data: IDataSlice;
+};
 
 export type AppDispatch = typeof store.dispatch;
